374a4f168c0faa8a4997177100fdbba01f7bd46c
Manasa Ranjan Mishra <manasaranjanmishra@wfos-MacBook-Air.local>
2020-02-15 21:56:52 +0530

    code updated

--- jobqueue.js
+++ jobqueue.js
@@ -2,37 +2,30 @@ function createJobQueue() {
   // TODO - create and return a job queue
   let jobQueue = [];
   return {
-    addJob: function(job) {
-      jobQueue.push(
-        new Promise(async (resolve, reject) => {
-          try {
-            let res = await job();
-            resolve(res);
-          } catch (error) {
-            reject(error);
-          }
-        })
-      )
-      return jobQueue[jobQueue.length-1]
+    addJob: function (job) {
+      var wp = new Promise(async (resolve, reject) => {
+        jobQueue.push({ job, resolve, reject })
+      });
+      return wp;
     },
-    processAllJobs : () => {
-      return new Promise((resolve, reject) => {
-        Promise.allSettled(jobQueue).then(res => {
-          resolve(res.length);
-        })
-      })
-      
+    processAllJobs: async () => {
+      let count = 0;
+      for (let item of jobQueue) {
+        try {
+          var result = await item.job()
+          item.resolve(result);
+          count++
+        } catch (error) {
+          item.reject(error);
+        }
+      }
+      return count;
     },
-    cancelJob: (job)=> {
-      let index = jobQueue.findIndex((ele, index) => {
-        if(ele.toString() == job.toString()) {
-          return index;
-        }});
-      if(index>-1) {
-        console.log('index is:', index)
-        jobQueue.splice(index,1)
-        return Promise.reject(job);
-        
+    cancelJob: (job) => {
+      let index = jobQueue.findIndex((ele) => ele.job == job.toString())
+      if (index > -1) {
+        var ele = jobQueue.splice(index, 1)
+        return ele[0].reject();
       }
     }
   }
--- tests.js
+++ tests.js
@@ -10,139 +10,133 @@ describe('Job Queue / Read-Only Tests', function() {
 
     it('adds a job to the queue and returns a promise that resolves with the value returned by the job', async function() {
       const jobPromise = jobQueue.addJob(async () => 42);
-      console.log('jobPromise is:', jobPromise)
       await jobQueue.processAllJobs();
-      console.log('jobPromise is:', jobPromise)
       expect(await jobPromise).to.equal(42);
     });
 
-    // it('adds a job to the queue and returns a promise that resolves with the value returned by the job', async function() {
-    //   let executed = 0;
-    //   const jobPromise = jobQueue.addJob(async () => executed = 1);
-    //   console.log('jobPromise is:', jobPromise)
-    //   // await jobQueue.processAllJobs();
-    //   console.log('jobPromise is:', jobPromise)
-    //   expect(executed).to.equal(1);
-    // });
+    it('adds a job to the queue and returns a promise that resolves with the value returned by the job', async function() {
+      let executed = 0;
+      const jobPromise = jobQueue.addJob(async () => executed = 1);
+      await jobQueue.processAllJobs();
+      expect(executed).to.equal(1);
+    });
 
-    // it('returns a promise that rejects if job rejects', async function() {
-    //   const jobPromise = jobQueue.addJob(async () => {
-    //     throw new Error('Ah ah ah, you didn\'t say the magic word!');
-    //   });
-    //   await jobQueue.processAllJobs();
+    it('returns a promise that rejects if job rejects', async function() {
+      const jobPromise = jobQueue.addJob(async () => {
+        throw new Error('Ah ah ah, you didn\'t say the magic word!');
+      });
+      await jobQueue.processAllJobs();
 
-    //   return jobPromise
-    //     .then(() => {
-    //       throw new Error('should have rejected');
-    //     })
-    //     .catch(error => {
-    //       expect(true).to.be.true;
-    //     });
-    // });
+      return jobPromise
+        .then(() => {
+          throw new Error('should have rejected');
+        })
+        .catch(error => {
+          expect(true).to.be.true;
+        });
+    });
   });
 
-  // describe('cancelJob', function() {
-  //   let jobQueue;
-  //   beforeEach(function () {
-  //     jobQueue = createJobQueue();
-  //   });
+  describe('cancelJob', function() {
+    let jobQueue;
+    beforeEach(function () {
+      jobQueue = createJobQueue();
+    });
 
-  //   it('removes a job from the queue', async function() {
-  //     let executed = false;
-  //     const job = async () => {
-  //       executed = true;
-  //     };
+    it('removes a job from the queue', async function() {
+      let executed = false;
+      const job = async () => {
+        executed = true;
+      };
 
-  //     const jobPromise = jobQueue.addJob(job);
-  //     console.log('jobPromise is:', jobPromise)
-  //     console.log('executed is:', executed)
-  //     jobQueue.cancelJob(job);
+      const jobPromise = jobQueue.addJob(job);
+      jobQueue.cancelJob(job);
 
-  //     await jobQueue.processAllJobs();
-  //     expect(executed).to.equal(false);
+      await jobQueue.processAllJobs();
+      expect(executed).to.equal(false);
 
-  //     return jobPromise.catch(() => {
-  //       // avoid unhandled promise rejection warning
-  //     });
-  //   });
+      return jobPromise.catch(() => {
+        // avoid unhandled promise rejection warning
+      });
+    });
 
-  //   it('rejects the promise returned by addJob', function() {
-  //     const job = async () => 42;
-  //     const jobPromise = jobQueue.addJob(job);
-  //     jobQueue.cancelJob(job);
+    it('rejects the promise returned by addJob', function() {
+      const job = async () => 42;
+      const jobPromise = jobQueue.addJob(job);
+      jobQueue.cancelJob(job);
 
-  //     return jobPromise
-  //       .then(() => {
-  //         throw new Error('should have rejected');
-  //       })
-  //       .catch(error => {
-  //         expect(true).to.be.true;
-  //       });
-  //   });
+      return jobPromise
+        .then(() => {
+          throw new Error('should have rejected');
+        })
+        .catch(error => {
+          expect(true).to.be.true;
+        });
+    });
 
-  //   it('does nothing if no matching job is found', async function() {
-  //     jobQueue.addJob(async () => 41);
-  //     jobQueue.addJob(async () => 42);
-  //     jobQueue.cancelJob(async () => 43);
-  //     const numProcessed = await jobQueue.processAllJobs();
-  //     expect(numProcessed).to.equal(2);
-  //   });
-  // });
+    it('does nothing if no matching job is found', async function() {
+      jobQueue.addJob(async () => 41);
+      jobQueue.addJob(async () => 42);
+      jobQueue.cancelJob(async () => 43);
+      const numProcessed = await jobQueue.processAllJobs();
+      expect(numProcessed).to.equal(2);
+    });
+  });
 
-  // describe('processAllJobs', function() {
-  //   let jobQueue;
-  //   beforeEach(function () {
-  //     jobQueue = createJobQueue();
-  //   });
+  describe('processAllJobs', function() {
+    let jobQueue;
+    beforeEach(function () {
+      jobQueue = createJobQueue();
+    });
 
-  //   it('processes all jobs, in order, and resolves with the number of jobs successfully processed', async function() {
-  //     let output = '';
-  //     jobQueue.addJob(async () => output += '1');
-  //     jobQueue.addJob(async () => output += '2');
-  //     jobQueue.addJob(async () => output += '3');
-  //     const numProcessed = await jobQueue.processAllJobs();
-  //     expect(output).to.equal('123');
-  //     expect(numProcessed).to.equal(3);
-  //   });
+    it('processes all jobs, in order, and resolves with the number of jobs successfully processed', async function() {
+      let output = '';
+      jobQueue.addJob(async () => output += '1');
+      jobQueue.addJob(async () => output += '2');
+      jobQueue.addJob(async () => output += '3');
+      const numProcessed = await jobQueue.processAllJobs();
+      expect(output).to.equal('123');
+      expect(numProcessed).to.equal(3);
+    });
 
-  //   it('does not process more than one job at a time', async function() {
-  //     let isProcessing = false;
+    it('does not process more than one job at a time', async function() {
+      let isProcessing = false;
 
-  //     const executeOneJob = async () => {
-  //       expect(isProcessing).to.be.false;
-  //       isProcessing = true;
+      const executeOneJob = async () => {
+        expect(isProcessing).to.be.false;
+        isProcessing = true;
 
-  //       await new Promise(resolve => {
-  //         setImmediate(() => {
-  //           isProcessing = false;
-  //           resolve();
-  //         });
-  //       });
-  //     };
+        await new Promise(resolve => {
+          setImmediate(() => {
+            isProcessing = false;
+            resolve();
+          });
+        });
+      };
 
-  //     jobQueue.addJob(async () => executeOneJob());
-  //     jobQueue.addJob(async () => executeOneJob());
-  //     jobQueue.addJob(async () => executeOneJob());
+      jobQueue.addJob(async () => executeOneJob());
+      jobQueue.addJob(async () => executeOneJob());
+      jobQueue.addJob(async () => executeOneJob());
 
-  //     const numProcessed = await jobQueue.processAllJobs();
-  //     expect(numProcessed).to.equal(3);
-  //   });
+      const numProcessed = await jobQueue.processAllJobs();
+      expect(numProcessed).to.equal(3);
+    });
 
-  //   it('does not reject if any of the jobs throw an error', async function() {
-  //     let output = '';
+    it('does not reject if any of the jobs throw an error', async function() {
+      let output = '';
 
-  //     jobQueue.addJob(async () => output += '1');
-  //     jobQueue.addJob(async () => output += '3');
+      jobQueue.addJob(async () => output += '1');
+      jobQueue.addJob(async () => output += '3');
 
-  //     const errorPromise = jobQueue.addJob(async () => {
-  //       throw new Error('this job does not work');
-  //     });
+      const errorPromise = jobQueue.addJob(async () => {
+        throw new Error('this job does not work');
+      });
 
-  //     await jobQueue.processAllJobs();
-  //     expect(output).to.equal('13');
-  //     return errorPromise.catch(() => {
-  //       // avoid unhandled promise rejection warning
-  //     });
-  //   });
-  // });
+      await jobQueue.processAllJobs();
+      expect(output).to.equal('13');
+      return errorPromise.catch(() => {
+        // avoid unhandled promise rejection warning
+      });
+    });
+  });
 });
